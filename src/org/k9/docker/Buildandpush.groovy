a616e11335e5948938cd39c995874aafe0f2a732
tkmajra <Mohammed.Shoaib@kohls.com>
2018-01-11 19:50:41 +0530

    fixed code

--- src/org/k9/docker/Buildandpush.groovy
+++ src/org/k9/docker/Buildandpush.groovy
@@ -1,24 +1,31 @@
 package org.k9.scm
 import org.k9.*
 
-class BuildDockerImage implements Serializable {
-  def dockerimageurl
-  def projectname
-  def dockerimagebranch
+class BuildDockerImage {
+  // def dockerimageurl
+  // def projectname
+  // def dockerimagebranch
+  // ocregistry, appname, projectname,
 
-  BuildDockerImage(dockerimageurl, projectname, dockerimagebranch) {
-    this.dockerimageurl = dockerimageurl
-    this.projectname = projectname
-    this.dockerimagebranch = dockerimagebranch
+  def config
+  def script
+
+  BuildDockerImage(script, config) {
+    this.config = config
+    this.script = script
   }
 
   void buildandpushDocker(){
-    this.script.stage('Pulling Docker Image')   
-    new scm.Git(this,config).checkout(this.dockerimageurl,this.dockerimageurl);
-    this.script.stage('Building and Pushing Docker Image to OCR')   
-    this.script.sh('docker build -t springboot-app .')
-    this.script.sh('docker tag springboot-app 172.19.1.1.:5000/springboot-app/springboot-app')
-    this.script.sh('docker push 172.19.1.1.:5000/springboot-app/springboot-app')
+    this.script.stage('Building & Deploying Docker Image') {
+    new scm.Git(this,config).checkout(this.config.dockerimageurl, this.config.dockerimagebranch);
+    // this.script.stage('Building and Pushing Docker Image to OCR')
+  //  this.script.sh('docker build -t springboot-app .')
+    this.script.sh('docker build -t this.config.appname .')
+    // this.script.sh('docker tag springboot-app 172.19.1.1:5000/springboot-app/springboot-app')
+    this.script.sh('docker tag this.config.appname this.config.ocregistry/this.config.appname')
+    // this.script.sh('docker push 172.19.1.1:5000/springboot-app/springboot-app')
+    this.script.sh('docker push this.config.ocregistry')
+  }
     //format: openshift cluster ip/projectname/app name
     //this.script.sh('docker build .')
 
--- src/org/k9/docker/Ocdeploy.groovy
+++ src/org/k9/docker/Ocdeploy.groovy
@@ -1,18 +1,21 @@
 package org.k9.scm
 import org.k9.*
 
-class Ocdeploy implements Serializable {
-  def dockerimagename
-  def appname
-  def projectname
+class Ocdeploy {
+  //def dockerimagename
+  //def appname
+  //def projectname
+  def config
+  def script
 
-  Ocdeploy(dockerimagename,appname, projectname) {
-    this.dockerimagename = dockerimagename
-    this.appname = appname
-    this.projectname = projectname
-  }
+  Ocdeploy(script, config) {
+      this.config = config
+      this.script = script
+    }
 
   void deploy(){
-    this.script.stage('Starting Deployment on OC')      
-    this.script.sh('oc this.appname this.dockerimagename')
+    this.script.stage('Starting Deployment on OC') {
+    this.script.sh('oc this.config.appname this.config.dockerimagename')
+  //  this.script.sh('oc this.appname this.dockerimagename')
+    }
 }
