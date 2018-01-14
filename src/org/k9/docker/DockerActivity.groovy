2ae13e163659eeeefbb5ead9e7b63229b223dbfa
root <root@templateservice.localdomain>
2018-01-15 03:02:22 +0530

    checking

--- src/org/k9/docker/DockerActivity.groovy
+++ src/org/k9/docker/DockerActivity.groovy
@@ -19,7 +19,7 @@ class DockerActivity implements Serializable {
   void dpush() {
     this.script.stage('Pushing Image') {
         this.script.docker.withRegistry('https://registry.hub.docker.com', 'dlogin') {
-            this.script.sh("docker tag amydocker/maven:springboot maven:springboot")
+            this.script.sh("docker tag maven:springboot amydocker/maven:springboot")
             this.script.sh("docker push amydocker/maven:springboot")
         }
     }
