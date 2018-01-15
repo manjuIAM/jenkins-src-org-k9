b653d0816cb8eb4aaf95949a91489573efffc62f
root <root@templateservice.localdomain>
2018-01-15 09:02:23 +0530

    checking

---
+++ src/org/k9/openshift/Deploy.groovy
@@ -0,0 +1,17 @@
+package org.k9.openshift
+
+class Deploy implements Serializable {
+  def config
+  def script
+
+  Deploy(script, config) {
+    this.config = config
+    this.script = script
+  }
+
+  void deployFromDimage() {
+    this.script.stage('Deploying In OpenShift') {
+	this.script.sh("oc new-app docker.io/amydocker/springboot:latest --name=springboot -n atest1")
+    }
+  }
+}
